22c6718807db144aa1c19980b86fa4806aa8437b
WIRTEK\sebastian.pop <sebastian.pop@wirtek.com>
2018-11-13 17:33:39 +0200

    Added OOP_Principles homework

---
+++ Homeworks/Sebastian Pop/OOP_Principles/OOP_Principles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Principles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface ui = new UserInterface();
+            ui.Run();
+        }
+    }
+}
